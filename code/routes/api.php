9ce35c08c8f9b62034c6914d9abe1b61c11fb12f
haveacess <haveacess@gmail.com>
2022-12-16 19:08:59 +0300

    Remove test config values

--- code/routes/api.php
+++ code/routes/api.php
@@ -17,11 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/devil', function (Request $request) {
-    return sprintf('Hello world. My name is: %s', $request->get('name'));
-});
-
-Route::get('/test-env', function (Request $request) {
-    return sprintf('Env value is: %s', config('app.test_config_value'));
-});
